bcc753126c72e6e2af00cd2c0f2c625a06cf7cc4
Ni30shkumar <addy2166@gmail.com>
2023-12-09 02:10:11 +0530

    first

---
+++ variable.js
@@ -0,0 +1,11 @@
+const accountId = 14453
+let accountEmail = "niteesh@google.com"
+var accountPasword = "Fatehpur"
+
+// Modefiy
+// account = 4 is not allow to it
+accountEmail = "zdvvvvfd@google.com"
+accountPasword = "hajipur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPasword])
